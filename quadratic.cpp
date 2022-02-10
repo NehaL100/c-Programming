98212ffe4d0f16867ce9d9f0e6e54f2706fbe81d
Muhammad Nehal <99079068+NehaL100@users.noreply.github.com>
2022-02-10 14:41:20 +0500

    Add files via upload

---
+++ quadratic.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main (){
+   int l;
+   int b;
+   int area;
+   int r;
+   float c=3.14;
+   cout<<"enter length"<<endl;
+   cin>>l;
+   cout<< "enter breadth"<<endl;
+   cin>>b;
+   area=l*b;
+   cout<<"area of square is="<<area<<endl;
+   cout<<"enter radius"<<endl;
+   cin>>r;
+   c=3.14*r*r;
+   cout<<"circumference of circle is"<<c<<endl;
+   
+   return 0;}
+ 
